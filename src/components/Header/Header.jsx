1cdfb2b12394a0cce99fdd169cdefb5c833a8445
Maksim Bilukha <bilukha.maksim@student.ehu.lt>
2026-06-03 15:49:40 +0300

    React hw 2 (#2)
    * HW-1: arbitrary list
    
    * fix
    
    * HW2
    
    * HW2-fix

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import Logo from '../../assets/HeaderPics/Logo.png' 
+import Cart from '../../assets/HeaderPics/Group.svg' 
+import styles from './Header.module.css';
+
+const links = [
+    {id: 1, label: "Home", href: "#"},
+    {id: 2, label: "Menu", href: "#"},
+    {id: 3, label: "Company", href: "#"},
+    {id: 4, label: "Login", href: "#"}
+]
+
+export default function Header() {
+    return (
+        <header className={styles.Header}>
+
+            <img src={Logo} alt="Logo" className={styles.LogoPic}/>
+
+            <div className={styles.HeaderLinks}>
+                <nav>
+                    <ul className={styles.NavList}>
+                        {links.map(link => (
+                            <li key={link.id}>
+                                <a href={link.href}>{link.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+
+            <div>
+                <button className={styles.CartButton} aria-label="Cart">
+                    <img src={Cart} alt="" className={styles.CartPic}/>
+                </button>
+            </div>
+
+        </header>
+    )
+
+}

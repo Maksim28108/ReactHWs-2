0c2c2976a5bea83073e47fbada0a827fc8201a91
Maksim28108 <bilukha.maksim@student.ehu.lt>
2026-06-29 13:41:30 +0300

    fix

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,5 +3,5 @@ import { Navigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  return isLoggedIn ? children : <Navigate to="/" />;
+  return isLoggedIn ? children : <Navigate to="/login" replace />;
 }
